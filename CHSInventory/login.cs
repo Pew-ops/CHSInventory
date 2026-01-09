395821d5912590bc2edb9b3b814eb85b2c86c1b1
Brian Dael <your-brianmaygdael@gmail.com>
2026-01-09 16:47:19 +0800

    nursepanel,stock,dispence medicine,patients history

--- CHSInventory/login.cs
+++ CHSInventory/login.cs
@@ -3,11 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
 using MySql.Data.MySqlClient;
@@ -107,9 +102,6 @@ namespace CHSInventory
                                     dashboard = new Admin1();
                                     break;
 
-                                case "Nurses":
-                                    dashboard = new Nurses();
-                                    break;
 
                                 case "STA":
                                     dashboard = new Sta();
